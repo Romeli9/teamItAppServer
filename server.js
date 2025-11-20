df8f2427a7bbc6226290737bc27d7dfb687c6dd4
Romeli9 <119035936+Romeli9@users.noreply.github.com>
2025-11-20 16:16:47 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 require('dotenv').config();
 const express = require('express');
 const multer = require('multer');
@@ -5,6 +6,8 @@ const Minio = require('minio');
 const cors = require('cors');
 const path = require('path');
 const crypto = require('crypto');
+const https = require('https');
+const fs = require('fs');
 
 const app = express();
 app.use(cors());
@@ -25,9 +28,7 @@ const minioClient = new Minio.Client({
 });
 
 minioClient.bucketExists(BUCKET_NAME, function (err, exists) {
-  if (err) {
-    return console.error('Ошибка проверки бакета:', err);
-  }
+  if (err) return console.error('Ошибка проверки бакета:', err);
   if (!exists) {
     minioClient.makeBucket(BUCKET_NAME, 'us-east-1', function (err) {
       if (err) return console.error('Ошибка создания бакета:', err);
@@ -41,27 +42,22 @@ minioClient.bucketExists(BUCKET_NAME, function (err, exists) {
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
-// Эндпоинт для загрузки файла
 app.post('/upload', upload.single('file'), (req, res) => {
   if (!req.file) return res.status(400).json({ error: 'No file uploaded' });
 
   const ext = path.extname(req.file.originalname);
   const fileName = crypto.randomBytes(16).toString('hex') + ext;
 
-  minioClient.putObject(BUCKET_NAME, fileName, req.file.buffer, req.file.size, async (err) => {
+  minioClient.putObject(BUCKET_NAME, fileName, req.file.buffer, req.file.size, (err) => {
     if (err) return res.status(500).json({ error: 'Upload failed' });
-    console.log(fileName);
     res.json({ id: fileName });
   });
 });
 
-// Эндпоинт для получения ссылки по id
 app.get('/file/:id', async (req, res) => {
   const fileId = req.params.id;
-
   try {
-    const presignedUrl = await minioClient.presignedGetObject(BUCKET_NAME, fileId, 24 * 60 * 60); // 24 часа
-    console.log(presignedUrl);
+    const presignedUrl = await minioClient.presignedGetObject(BUCKET_NAME, fileId, 24 * 60 * 60);
     res.json({ url: presignedUrl });
   } catch (err) {
     console.error(err);
@@ -69,8 +65,14 @@ app.get('/file/:id', async (req, res) => {
   }
 });
 
-// Запуск сервера
+
+// HTTPS сервер с настоящим сертификатом
+const options = {
+  key: fs.readFileSync('/etc/letsencrypt/live/teamitserver.ru/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/teamitserver.ru/fullchain.pem'),
+};
+
 const PORT = Number(process.env.PORT) || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running`);
+https.createServer(options, app).listen(PORT, '0.0.0.0', () => {
+  console.log(`HTTPS Server running on https://0.0.0.0:${PORT}`);
 });
